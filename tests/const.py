8aa82163205a3cc5eaf543ff0275a8c121bd4a9f
Erik Raae <erik@raae.eu>
2022-10-29 22:28:00 +0000

    Incorporate tests at a later stage.

--- tests/const.py
+++
@@ -1,5 +0,0 @@
-"""Constants for integration_blueprint tests."""
-from custom_components.hra_recycling.const import CONF_ADDRESS_ID, CONF_ADDRESS
-
-# Mock config data to be used across multiple tests
-MOCK_CONFIG = {CONF_ADDRESS: "test_username", CONF_ADDRESS_ID: "test_password"}

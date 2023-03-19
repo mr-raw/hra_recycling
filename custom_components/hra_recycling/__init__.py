6c0bd3707939bd2fb4fce9dfe303acea998c1557
Erik Raae <erik@raae.eu>
2023-03-19 11:43:53 +0000

    Fixed linting error in __init__ file

--- custom_components/hra_recycling/__init__.py
+++ custom_components/hra_recycling/__init__.py
@@ -4,7 +4,7 @@ import logging
 from datetime import timedelta
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import Config, HomeAssistant
+from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed

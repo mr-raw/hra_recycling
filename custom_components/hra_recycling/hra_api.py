6f2035c57a77036ee6782a9287b7a287100aa995
Erik Raae <erik@raae.eu>
2023-03-19 21:16:28 +0000

    Renamed the api file

--- custom_components/hra_recycling/hra_api.py
+++ custom_components/hra_recycling/hra_api.py
@@ -1,4 +1,4 @@
-"""HRA API Client"""
+"""HRAapi.py"""
 import logging
 import asyncio
 import socket
@@ -12,28 +12,39 @@ _LOGGER: logging.Logger = logging.getLogger(__package__)
 HEADERS = {"Content-type": "application/json; charset=UTF-8"}
 
 
+class ApiClientError(Exception):
+    """Exception to indicate a general API error."""
+
+
+class ApiClientCommunicationError(ApiClientError):
+    """Exception to indicate a communication error."""
+
+
+class ApiClientAuthenticationError(ApiClientError):
+    """Exception to indicate an authentication error."""
+
+
 class ApiClient:
     """ApiClient()"""
 
     def __init__(self, address: str, session: aiohttp.ClientSession) -> None:
         """HRA API Client"""
-        self.address = address  # This your street address
-        self.session = session
+        self._address = address  # This your street address
+        self._session = session
         self.agreement_id = ""
 
-    async def async_verify_address(self) -> list:
+    async def async_verify_address(self) -> str:
         """Verify that the provided address is valid."""
+        url = f"https://api.hra.no/search/address?query={self._address}"
+        data = await self._get_agreement_id_from_address(url)
+        return data
 
-        url = f"https://api.hra.no/search/address?query={self.address}"
-
-        return await self._get_agreement_id_from_address(url)
-
-    async def _get_agreement_id_from_address(self, url: str) -> list:
+    async def _get_agreement_id_from_address(self, url: str) -> str:
         """Get information from the API."""
 
         try:
             async with async_timeout.timeout(TIMEOUT):
-                resp = await self.session.get(url)
+                resp = await self._session.get(url)
             return await resp.json()
 
         except asyncio.TimeoutError as exception:
@@ -59,5 +70,17 @@ class ApiClient:
         except Exception as exception:  # pylint: disable=broad-except
             _LOGGER.error("Something really wrong happened! - %s", exception)
 
-    async def async_update_data(self) -> list:
-        """Updates the data"""
+    async def async_retrieve_fraction_data(self):
+        """Get fraction data"""
+        agreement_id = self.agreement_id
+        data = await self._get_fraction_data(agreement_id)
+        return data
+
+    async def _get_fraction_data(self, uid: str):
+        """Actually retrieve data"""
+        print(uid)
+
+    @property
+    def address(self):
+        """Getter for the address"""
+        return self._address
